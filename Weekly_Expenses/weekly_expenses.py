ef4747a96b08fc8f0155a9cb4eec1466d96016e6
Nilesh M <nilesh496@gmail.com>
2024-10-03 00:24:14 +0530

    code updated

--- Weekly_Expenses/weekly_expenses.py
+++ Weekly_Expenses/weekly_expenses.py
@@ -4,7 +4,7 @@ Total_expenses=0
 for i in range(7):
     day_expenses = int(input(f"Expenses on Day {i+1} :"))
     Total_expenses+=day_expenses
-print("Total_expenses :", Total_expenses)
+print("Total_expenses: ", Total_expenses)
 
 if Total_expenses > Target:
     print("You have exceeded the weekly Limit !!")
