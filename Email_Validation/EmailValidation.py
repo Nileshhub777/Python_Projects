e3057d77bcea3db7728340d4e5b10dfeac8f7fba
Nilesh M <nilesh496@gmail.com>
2024-10-05 20:01:40 +0530

    email validation

---
+++ Email_Validation/EmailValidation.py
@@ -0,0 +1,33 @@
+email=input("Enter the email id :")
+k,j,d = 0,0,0
+if len(email) > 6:
+    if email[0].isalpha():
+        if '@' in email and email.count('@') == 1:
+            if (email[-4] == ".")  ^ (email[-3] == "."):
+                for i in email:
+                    if i==i.isspace(): #5
+                        k=1
+                    elif i.isalpha(): #5
+                        if i==i.upper():
+                            j=1
+                    elif i.isdigit():
+                        continue
+                    elif i=="_" or i == "." or i == "@":
+                        continue
+                    else: #5
+                        d=1
+
+
+                if k==1 or j ==1 or d == 1:
+                    print("Invalid email 5")
+                else:
+                    print("Correct email")
+            else:
+                print("Wrong email id 4")
+        else:
+            print("Wrong email id 3")
+
+    else:
+        print("Wrong email 2")
+else:
+    print("Invalid email address. 1")
