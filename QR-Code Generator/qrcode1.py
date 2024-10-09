1528d7c2d49e933d8349f1d834516016e2e3376c
Nilesh M <nilesh496@gmail.com>
2024-10-09 23:54:05 +0530

    qrcode latest

--- QR-Code Generator/qrcode1.py
+++ QR-Code Generator/qrcode1.py
@@ -2,4 +2,6 @@ import qrcode as qr
 
 image1=qr.make('https://www.youtube.com/watch?v=gadjsB5BkK4')
 
+
+
 image1.save("New_Youtube.png")
---
+++ QR-Code Generator/qrcode2.py
@@ -0,0 +1,5 @@
+import qrcode as qr
+
+image2=qr.make('https://www.youtube.com/watch?v=gud65lqebrc&t=95s')
+
+image2.save("New2_Youtube.png")
