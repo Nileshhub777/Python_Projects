9cfbe569809c6afbfec7907784ca7d71419df984
Nilesh M <nilesh496@gmail.com>
2024-10-29 22:41:05 +0530

    todo

---
+++ TODO/todo.py
@@ -0,0 +1,30 @@
+tasks = []
+
+while True:
+    print("\nTo-Do List App")
+    print("1. Add task")
+    print("2. Remove task")
+    print("3. View tasks")
+    print("4. Exit")
+    choice = input("Choose an option: ")
+
+    if choice == '1':
+        task = input("Enter a new task: ")
+        tasks.append(task)
+        print("Task added!")
+    elif choice == '2':
+        task = input("Enter the task to remove: ")
+        if task in tasks:
+            tasks.remove(task)
+            print("Task removed!")
+        else:
+            print("Task not found!")
+    elif choice == '3':
+        print("\nYour Tasks:")
+        for i, task in enumerate(tasks, 1):
+            print(f"{i}. {task}")
+    elif choice == '4':
+        print("Exiting...")
+        break
+    else:
+        print("Invalid choice. Try again.")
