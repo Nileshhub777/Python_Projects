b8f2e22d5e635079f4b51466bee522ba5ce78890
Nilesh M <nilesh496@gmail.com>
2024-11-05 22:05:29 +0530

    countdowntimer

---
+++ Countdown/countdown.py
@@ -0,0 +1,12 @@
+import time
+
+def countdown(seconds):
+    while seconds > 0:
+        mins, secs = divmod(seconds, 60)
+        timer = f"{mins:02}:{secs:02}"
+        print(timer, end="\r")
+        time.sleep(1)
+        seconds -= 1
+    print("Time's up!")
+
+countdown(10)  # Set countdown time in seconds
