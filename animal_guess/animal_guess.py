30c51bea37cbba6c8bec62b0c2d3c47f0d36a6a5
Nilesh M <nilesh496@gmail.com>
2024-12-07 20:08:04 +0530

    guess

--- animal_guess/animal_guess.py
+++ animal_guess/animal_guess.py
@@ -9,7 +9,7 @@ def check_guess(guess, answer):
             still_guessing = False
         else:
             if attempt < 2:
-                guess = input("Sorry Wrong Answer, you should try again")
+                guess = input("Sorry Wrong Answer, please try again")
             attempt = attempt + 1
     if attempt == 3:
         print("The Correct answer is ", answer)
