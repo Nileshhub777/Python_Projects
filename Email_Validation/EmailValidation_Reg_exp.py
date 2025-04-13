f156aff2131ba48896a194f83843008768dfb09a
Nilesh M <nilesh496@gmail.com>
2025-04-13 20:02:32 +0530

    emailvalidation1

--- Email_Validation/EmailValidation_Reg_exp.py
+++ Email_Validation/EmailValidation_Reg_exp.py
@@ -4,6 +4,6 @@ email_condition="^[a-z]+[\._]?[a-z 0-9]+[@]\w+[.]\w{2,3}$"
 user_email=input("Enter email address:")
 
 if re.search(email_condition,user_email):
-    print("It's the Correct valid email format")
+    print("It's the valid valid email format")
 else:
     print("It's the invalid format of email")
