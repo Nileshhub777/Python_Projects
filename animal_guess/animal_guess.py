556c94bbddf0d288cf3c9dd1626e26d998f5ae8e
Nilesh M <nilesh496@gmail.com>
2025-04-16 20:39:40 +0530

    animal guess

--- animal_guess/animal_guess.py
+++ animal_guess/animal_guess.py
@@ -19,7 +19,7 @@ score = 0
 print("Let's guess the Animal !!")
 guess1 = input("Which bear lives at the North Pole ? ")
 check_guess(guess1, "Polar bear")
-guess2 = input("Which is fastest land animal in the world ? ")
+guess2 = input("Who is fastest land animal in the world ? ")
 check_guess(guess2, "Cheetah")
 guess3 = input("Which is the largest animal ? ")
 check_guess(guess3, "Blue Whale")
