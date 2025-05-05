02b36a5bb804b324bb52e0a8a8aef80d6c3f39d7
Nilesh M <nilesh496@gmail.com>
2025-05-05 15:09:11 +0530

    text-gen-ai_try-catch

--- TextGenerator-AI/Text_Gen_AI.py
+++ TextGenerator-AI/Text_Gen_AI.py
@@ -1,16 +1,15 @@
 import requests
 def fetch_quote():
-    response=requests.get("https://zenquotes.io/api/random")
-
-    if response.status_code == 200:
-
-        #print(response.json())
-        data=response.json()[0]
-        #print(data)
-        quote=data['q']
-        author=data['a']
-        print(f"{quote} - {author}")
-    else:
-        print("Failed to fetch")
-        print(response.status_code)
+    try:
+        response=requests.get("https://zenquotes.io/api/random")
+        if response.status_code == 200:
+            data=response.json()[0]
+            quote=data['q']
+            author=data['a']
+            print(f"{quote} - {author}")
+        else:
+            print("Failed to fetch")
+            print(response.status_code)
+    except requests.exceptions.RequestException as e:(
+                print("Network Error :", e))
 fetch_quote()
