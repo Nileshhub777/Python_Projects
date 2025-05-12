08cf2fbb3c60218501d96ee4cce59e91eb196b9d
Nilesh M <nilesh496@gmail.com>
2025-05-12 23:25:55 +0530

    sumofeven

--- sumofevens/sumofeven.py
+++ sumofevens/sumofeven.py
@@ -2,7 +2,7 @@ def sumofevens(numbers):
     return sum(num for num in numbers if num % 2 == 0)
 
 
-numbers = [1,2,3,4,5,6,7,8]
+numbers = [1,2,3,4,5,6,7,8,9,10,11]
 result = sumofevens(numbers)
 
 print("Sum result :",result)
