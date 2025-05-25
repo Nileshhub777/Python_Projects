c869bff4389383885a28db19768f5d9eb4aac0d2
Nilesh M <nilesh496@gmail.com>
2025-05-25 12:14:58 +0530

    password

--- Pass_Generator/PasswordGenerator.py
+++ Pass_Generator/PasswordGenerator.py
@@ -2,14 +2,14 @@
 import random
 
 #Asking user to input the length of the password to be generated
-pass_len=int(input("Enter the length of the password :"))
+pass_len=int(input("Enter the length of password :"))
 
 # Define possible characters manually in a simple way
 pass_data = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*?"
 
 # Check if the requested length exceeds the available characters
 if pass_len > len(pass_data):
-    print("The length of the password cannot exceed more than:",len(pass_data),"characters.")
+    print("The length of the password cannot exceed more than :",len(pass_data),"characters.")
 
 else:
     pass_generate=''.join(random.sample(pass_data,pass_len))
