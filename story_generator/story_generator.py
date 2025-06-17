70e93afca2d9b6d7ddfaebc23467756968718d54
Nilesh M <nilesh496@gmail.com>
2025-06-17 18:37:51 +0530

    story

--- story_generator/story_generator.py
+++ story_generator/story_generator.py
@@ -2,9 +2,9 @@
 import random
 
 # Storing random data into lists to create story.
-when = ['A long time ago', 'Yesterday', 'Before even you were born', 'In future', 'Before Thanos arrived']
+when = ['A long time ago', 'Yesterday', 'Before even you were born', 'In the future', 'Before Thanos arrived']
 who = ['Shazam', 'Iron Man', 'Batman', 'Superman', 'Captain America']
-went = ['Angrut Asylum', 'Gotham City', 'Stark Tower', 'Bat Cave', 'Avengers HQ']
+went = ['Angryt Asylum', 'Gotham City', 'Stark Tower', 'Bat Cave', 'Avengers HQ']
 what = ['to eat a lot of the cakes', 'to fight for justice', 'to steal the ice creams', 'to dance']
 
 # Using string concatenition & randome.choice() to print a random element from all the lists
